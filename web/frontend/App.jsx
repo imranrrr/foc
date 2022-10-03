a736778e6719ae3ae4c60be7644fc5c159a22ebe
floyd-home <wbunge@avanfeel.com>
2022-10-03 13:11:12 -0300

    nav menu

--- web/frontend/App.jsx
+++ web/frontend/App.jsx
@@ -21,9 +21,39 @@ export default function App() {
             <NavigationMenu
               navigationLinks={[
                 {
-                  label: "Product Page",
-                  destination: "/product",
-                }
+                  label: "Dashboard",
+                  destination: "/dashboard",
+                },
+                {
+                  label: "Suppliers",
+                  destination: "/suppliers",
+                },
+                {
+                  label: "Inbox",
+                  destination: "/inbox",
+                },
+                {
+                  label: "Stocktakes",
+                  destination: "/stocktakes",
+                },
+                {
+                  label: "Receiving",
+                  destination: "/receiving",
+                },
+                {
+                  label: "Purchase Orders",
+                  destination: "/purchase-orders",
+                },
+                {
+                  label: "Inventory",
+                  destination: "/inventory",
+                },
+                {
+                  label: "Products",
+                  destination: "/products",
+                },
+                
+
               ]}
             />
             <Routes pages={pages} />
