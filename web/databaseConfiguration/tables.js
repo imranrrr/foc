f24b6c6e1f43d4f05c39a5d87be2a0f310ef621a
imranrrr <emranlateef786@gmail.com>
2022-10-05 14:13:52 -0400

    added field

--- web/databaseConfiguration/tables.js
+++ web/databaseConfiguration/tables.js
@@ -14,27 +14,28 @@ export default function tables() {
     }
   });
 
-  // knex1.schema.hasTable("comments").then(function (exists) {
-  //   // if (!exists) {
-  //   //   console.log("hh")
-  //   //   return knex1.schema.createTable("comments", function (t) {
-  //   //     t.increments("id").primary();
-  //   //     t.string("product_id", 30);
-  //   //     t.string("user_email", 30);
-  //   //     t.string("user_id", 30);
-  //   //     t.string("user_name", 30);
-  //   //     t.string("description", 100);
-  //   //     t.string("date", 100);
-  //   //     t.string("to", 40)
-  //   //   });
-  //   // }
+  knex1.schema.hasTable("comments").then(function (exists) {
+    if (!exists) {
+      console.log("hh")
+      return knex1.schema.createTable("comments", function (t) {
+        t.increments("id").primary();
+        t.string("product_id", 30);
+        t.string("user_email", 30);
+        t.string("user_id", 30);
+        t.string("user_name", 30);
+        t.string("description", 100);
+        t.string("date", 100);
+      });
+    }
   // if(exists){
   //   console.log("dropepppp################")
   //   knex1.schema.dropTable('comments')
   // }
     
 
-  // });
-  knex1.schema.dropTable('comments')
+  });
+  knex1.schema.alterTable('comments', function(t) {
+    t.string('to', 40) 
+  });
   
 }
